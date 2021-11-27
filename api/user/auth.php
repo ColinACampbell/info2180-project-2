2b4133ddb20efa54c3209b6dfa671852f4460760
Colin Campbell <prontobol@gmail.com>
2021-11-26 19:37:40 -0500

    User login

--- api/user/auth.php
+++ api/user/auth.php
@@ -2,14 +2,32 @@
 
 require "./../config/database-config.php";
 
-if (isset($_POST['email']) && isset($_POST['password']))
-{
-    // validate the user input
-    // login user
-    echo "Hello World";
-} else 
-{
-    $response = [];
-    $response['message'] = "Wrong Login";
-    echo json_encode($response);
+$response = [];
+if (isset($_POST['email']) && isset($_POST['password'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $login_query = "SELECT * FROM users WHERE email = :email";
+    $statement = $db_conn->prepare($login_query);
+    $statement->execute([
+        'email' => $email,
+    ]);
+    $result = $statement->fetchAll();
+    if (count($result) == 0) {
+        $response['message'] = "Email Not Found";
+    } else {
+        $user = $result[0];
+        $is_password_correct = password_verify($password, $user['password']);
+        if ($is_password_correct) {
+            $user['password'] = null;
+            $response['message'] = "User Found";
+            $response['user'] = $user;
+        } else {
+            $response['message'] = "Incorrect Password";
+        }
+    }
+} else {
+    $response['message'] = "Credentials are needed";
 }
+
+echo json_encode($response);
