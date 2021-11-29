7f080b534b1be3c4f65db4a32dd0038b271316aa
Deezum <beadleselena@gmail.com>
2021-11-29 15:45:19 -0500

    issues.s

--- issues.js
+++ issues.js
@@ -1,36 +1,29 @@
 window.onload = function(){
-    //let url = document.location.href;
     let progbtn = document.getElementById("progbtn");
     let closebtn = document.getElementById("closebtn");
-    //params = url.split('?')[1].split('&');
 
     progbtn.addEventListener('click',function(){
     
             const d = new Date();
-            const min = d.getMinutes();
             const hour = (d.getHours() + 24) % 12 || 12;
             if( d.getHours() >= 12){
             document.getElementById("updatedetail").innerHTML = "Last updated on "+ d.toDateString() + " at " + hour + ":" +  (d.getMinutes()<10? '0' : '') + d.getMinutes() + "PM";
-            alert("Issue marked in progress");
             }
             if( d.getHours() < 12){
-                document.getElementById("updatedetail").innerHTML = "Last updated on "+ d.toDateString() + " at " + hour + ":" + d.getMinutes() + "AM";
-                alert("Issue marked in progress");
+                document.getElementById("updatedetail").innerHTML = "Last updated on "+ d.toDateString() + " at " + hour + ":" +(d.getMinutes()<10? '0' : '') + d.getMinutes() + "AM";
                 }
         })
     
 
     closebtn.addEventListener('click',function(){
-             const d = new Date();
-            const hour = (d.getHours() + 24) % 12 || 12;
-            if( d.getHours() >= 12){
-            document.getElementById("updatedetail").innerHTML = "Last updated on "+ d.toDateString() + " at " + hour + ":" + d.getMinutes() + "PM";
-            alert("Issue marked in progress");
+        const d = new Date();
+        const hour = (d.getHours() + 24) % 12 || 12;
+        if( d.getHours() >= 12){
+        document.getElementById("updatedetail").innerHTML = "Last updated on "+ d.toDateString() + " at " + hour + ":" +  (d.getMinutes()<10? '0' : '') + d.getMinutes() + "PM";
+        }
+        if( d.getHours() < 12){
+            document.getElementById("updatedetail").innerHTML = "Last updated on "+ d.toDateString() + " at " + hour + ":" +(d.getMinutes()<10? '0' : '') + d.getMinutes() + "AM";
             }
-            if( d.getHours() < 12){
-                document.getElementById("updatedetail").innerHTML = "Last updated on "+ d.toDateString() + " at " + hour + ":" + d.getMinutes() + "AM";
-                alert("Issue marked in progress");
-                }
         })  
 
 
