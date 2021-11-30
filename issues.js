70f471ebf686780dedf96032df6416502c2934db
Deezum <beadleselena@gmail.com>
2021-11-30 18:44:09 -0500

    Edited

--- issues.js
+++ issues.js
@@ -1,30 +1,48 @@
 window.onload = function(){
-    let progbtn = document.getElementById("progbtn");
-    let closebtn = document.getElementById("closebtn");
-
-    progbtn.addEventListener('click',function(){
-    
-            const d = new Date();
-            const hour = (d.getHours() + 24) % 12 || 12;
-            if( d.getHours() >= 12){
-            document.getElementById("updatedetail").innerHTML = "Last updated on "+ d.toDateString() + " at " + hour + ":" +  (d.getMinutes()<10? '0' : '') + d.getMinutes() + "PM";
-            }
-            if( d.getHours() < 12){
-                document.getElementById("updatedetail").innerHTML = "Last updated on "+ d.toDateString() + " at " + hour + ":" +(d.getMinutes()<10? '0' : '') + d.getMinutes() + "AM";
-                }
-        })
+    var url = document.location.href;
+    var progbtn = document.getElementById("progbtn");
+    var closebtn = document.getElementById("closebtn");
+    var xhttp = new XMLHttpRequest();
     
 
-    closebtn.addEventListener('click',function(){
-        const d = new Date();
-        const hour = (d.getHours() + 24) % 12 || 12;
-        if( d.getHours() >= 12){
-        document.getElementById("updatedetail").innerHTML = "Last updated on "+ d.toDateString() + " at " + hour + ":" +  (d.getMinutes()<10? '0' : '') + d.getMinutes() + "PM";
+    xhttp.open("GET",url, true);
+    xhttp.send();
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState == 4 && xhttp.status == 200){
+            let listinfo = JSON.parse(xhttp.responseText);
+            let title = document.getElementById("title");
+            let body = document.getElementById("paragraph");
+            let issue = document.getElementById("issue");
+            let dates = document.getElementById("dateinfo");
+            let aside = document.getElementById("asidemain");
+            title.innerHTML = listinfo["title"];
+            body.innerHTML = listinfo["description"];
+            issue.innerHTML = "Issue# " + listinfo["id"];
+            dates.innerHTML = "Issue is created on" + listinfo["created"] + "<br>" + "Issues was last updated on " + listinfo["updated"];
+            aside.innerHTML = "Assigned To: " + listinfo["assigned_to"] + "<br> Type: " + listinfo["type"] + "<br> Priority:" + listinfo["priority"] + "<br>Status: " + listinfo["status"];
+
         }
-        if( d.getHours() < 12){
-            document.getElementById("updatedetail").innerHTML = "Last updated on "+ d.toDateString() + " at " + hour + ":" +(d.getMinutes()<10? '0' : '') + d.getMinutes() + "AM";
-            }
-        })  
+    }
 
+    progbtn.addEventListener("click",function(){
+        xhttp1 = new XMLHttpRequest;
+        xhttp1.open("GET",url, true);
+        xhttp1.send();
+        if (xhttp1.readyState == 4 && xhttp1.status == 200){
+            alert("Issue marked in progress");
 
-}
+        }
+    })
+
+    closebtn.addEventListener("click",function(){
+        xhttp2 = new XMLHttpRequest;
+        xhttp2.open("GET",url, true);
+        xhttp2.send();
+        if (xhttp2.readyState == 4 && xhttp2.status == 200){
+            alert("Issue marked as closed");
+            
+
+        }  
+    })
+
+} 
