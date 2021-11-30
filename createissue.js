ec9f9b449a3aff4d5b1e77e172500f33e43e9d71
Deezum <beadleselena@gmail.com>
2021-11-30 18:43:43 -0500

    Edited

--- createissue.js
+++ createissue.js
@@ -7,7 +7,7 @@ function listenevent(){
     let submit = document.querySelector(".btn");
     
     submit.addEventListener('click', function(event){
-        formissue(); event.preventDefault(); 
+        formissue(); isValid(); getdata(); event.preventDefault(); 
         
         
     }) 
@@ -40,6 +40,7 @@ function formissue(){
           //alert("Not empty description");      
 }
       if (type == "Blank" ||prior == "Blank" ||assigns == "Blank"){
+          count = 1;
           alert("All fields must be filled out. Please select appropriate options from all drop down menus.");
       }
 }
@@ -47,6 +48,7 @@ function formissue(){
         if (title != "" || description != ""){
             var regEx =  /^[0-9a-zA-Z]+$/;
             if(!(title.match(regEx)) || !(description.match(regEx))){
+                count = 1;
                 alert("Enter only alphanumeric characters into the description and/or title fields.");
 
             }
@@ -54,7 +56,63 @@ function formissue(){
 
 
     }
+    return count;
 }
+
+function isValid(){
+    if (formissue() == 0){
+        //alert("VALID ENTRY");
+        return true;
+        
+    }
+    else{
+        //alert("INVALID ENTRY: Please Check The Textfield(s) Hightlighted In Red.");
+        return false;
+        
+    }
+    
+}
+
+function getdata(){
+    var formdata1 ={assign: form.assign.value, type1: form.type1.value, priority: form.priority.value, title: form.title.value, description: form.description.value};
+        
+         if (isValid()=== true){
+        let loginForm = new FormData();
+        
+        loginForm.set("assignedTo", formdata1["assign"]);
+        loginForm.set("type", formdata1["type1"]);
+        loginForm.set("priority", formdata1["priority"]);
+        loginForm.set("title", formdata1["title"]);
+        loginForm.set("description",formdata1["description"]);
+        
+
+        fetch('http://localhost/info2180-project-2/issue/create.php', {
+            method: "POST", 
+            body: loginForm
+        })
+        .then(response => {
+            if(response.ok){
+                return response.text();
+            }
+            else{
+                throw new Error(`An error has occured: ${response.status}`);
+            }
+        })
+        .then((html) => {
+            console.log(html)
+          
+        })
+        .catch(err => {
+            console.log(err);
+        })
+
+        
+    }
+
+}
+
+
+
 
 
 
