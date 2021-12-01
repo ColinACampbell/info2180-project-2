a1db02d6177387e315d0d852b876e7f8ddeb9203
Colin Campbell <prontobol@gmail.com>
2021-11-30 20:25:27 -0500

    Added login js

--- js/login.js
+++ js/login.js
@@ -1,24 +1,27 @@
 window.onload = function(){
-    var email= document.getElementById('email');
-    var password = document.getElementById('password');
+    var emailInput= document.getElementsByName('email')[0];
+    var passwordInput = document.getElementsByName('password')[0];
+
     var submit = document.getElementById('submit');
-    var xhr = new XMLHttpRequest();
 
     submit.addEventListener('click', function(event){
         event.preventDefault();
-        var email = email.value;
-        var pass = password.value;
-        xhr.open('POST','/api/user/auth.php?user=' + user + "&pass=" + pass);
-        xhr.send();
-        xhr.onreadystatechange = function(){
-            if (xhr.readyState == 4 && httpRequest.status == 200){
-                if(xhr.responseText == "redirect"){
-                    window.location = 'homepage.html';
-                }else{
-                    alert(xhr.responseText);
-                }
-            }
-        }
+        var email = emailInput.value;
+        var pass = passwordInput.value;
+        const formData = new FormData();
+        formData.append('email',email);
+        formData.append('password',pass);
+        fetch('/api/user/auth.php',{
+            method:"POST",
+            body:formData,
+            credentials:'include' // for login
+        }).then(async(response)=>{
+            console.log(await response.json())
+            // Take the json check if sucess or not
+            // Redirect to login screen or show appropiate message
+        }).catch((error)=>{
+            console.log(error)
+        })
     })
 }
 
