4ed6da437f593a69e2e2a2d02aa73238b3e1bdc0
Jevon Forrest <jevonforrest77@gmail.com>
2021-11-30 22:38:46 -0500

    Updated JS

--- js/createUser.js
+++ js/createUser.js
@@ -1,29 +1,35 @@
 window.onload = function(){
     var emailInput= document.getElementsByName('email')[0];
     var passwordInput = document.getElementsByName('password')[0];
-    var submit = document.getElementById('submit');
+    var fNameInput = document.getElementsByName('fName')[0];
+    var lNameInput = document.getElementsByName('lName')[0];
 
+    var submit = document.getElementById('submit');
     submit.addEventListener('click', function(event){
         event.preventDefault();
         var email = emailInput.value;
         var pass = passwordInput.value;
+        var fName = fNameInput.value;
+        var lName = lNameInput.value;
         const formData = new FormData();
         formData.append('email',email);
         formData.append('password',pass);
-        fetch('/api/user/auth.php',{
+        formData.append('firstName',fName);
+        formData.append('lastName',lName);
+        fetch('/api/user/create.php',{
             method:"POST",
             body:formData,
-            credentials:'include'
         }).then(async(response)=>{
             const responseJson = await response.json();
-            if (responseJson.message == "User Found") {
+            if (responseJson.message == "User Created") {
+                alert("User Created!");
                 window.location = 'index.html';
             } else {
-                alert("User Not Found!");
+                alert("Unable to Create User");
             }
         }).catch((error)=>{
             console.log(error)
         })
     })
-}
 
+}
