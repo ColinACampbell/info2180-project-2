3444651cd3da9d1e8ce6a32d38ec210453e62647
Jevon Forrest <jevonforrest77@gmail.com>
2021-11-30 19:42:24 -0500

    JS files

---
+++ login.js
@@ -0,0 +1,24 @@
+window.onload = function(){
+    var email= document.getElementById('email');
+    var password = document.getElementById('password');
+    var submit = document.getElementById('submit');
+    var xhr = new XMLHttpRequest();
+
+    submit.addEventListener('click', function(event){
+        event.preventDefault();
+        var email = email.value;
+        var pass = password.value;
+        xhr.open('POST','http://localhost/Info2180-project-2/api/user/auth.php?user=' + user + "&pass=" + pass);
+        xhr.send();
+        xhr.onreadystatechange = function(){
+            if (xhr.readyState == 4 && httpRequest.status == 200){
+                if(xhr.responseText == "redirect"){
+                    window.location = 'homepage.html';
+                }else{
+                    alert(xhr.responseText);
+                }
+            }
+        }
+    })
+}
+
