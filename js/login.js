9c086d8bc5ea201d88dd2c297e372425012915be
Colin Campbell <prontobol@gmail.com>
2021-11-30 20:30:19 -0500

    Added comment

--- js/login.js
+++ js/login.js
@@ -16,9 +16,11 @@ window.onload = function(){
             body:formData,
             credentials:'include' // for login
         }).then(async(response)=>{
-            console.log(await response.json())
+            //console.log(await response.json())
             // Take the json check if sucess or not
             // Redirect to app home screen or show appropiate message
+            const responseJson = await response.json();
+            // responseJson.message use this to check if a user is valid
         }).catch((error)=>{
             console.log(error)
         })
