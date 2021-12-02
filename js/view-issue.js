bbce2bfb08341bca85073f917c787b15de9e4fc9
Colin Campbell <prontobol@gmail.com>
2021-12-02 17:27:09 -0500

    Can now update issue's status

--- js/view-issue.js
+++ js/view-issue.js
@@ -3,16 +3,38 @@ window.onload = function () {
     const urlSearchParams = new URLSearchParams(window.location.search);
     const params = Object.fromEntries(urlSearchParams.entries());
     const id = parseInt(params.id);
+
+    const statusCloseBtn = document.getElementById('status-close-btn');
+    const statusProgBtn = document.getElementById('status-prog-btn');
+
+    statusProgBtn.addEventListener('click',()=>{
+        updateIssueStatus(id,'INPROGRESS')
+    })
+
+    statusCloseBtn.addEventListener('click',()=>{
+        updateIssueStatus(id,'CLOSED')
+    })
+
+
     getIssue(id);
 }
+
+const updateIssueStatus = (id,newStatus) => {
+
+    const formData = new FormData();
+    formData.append('newStatus',newStatus);
+    fetch('/api/issue/mark.php?id='+id,{
+        method:"POST",
+        body: formData,
+    })
+}
+
 
 const getIssue = (id) => {
     fetch('/api/issue/get.php?id=' + id).then(async (response) => {
         const jsonReponse = await response.json();
         const issue = jsonReponse.issue
         populateIssue(issue)
-        console.log(issue);
-        //addDataToTable(issues)
     })
 }
 
