d80d73036dd6bc67ee7a686ed9cbba78b7c0bb54
Colin Campbell <prontobol@gmail.com>
2021-12-03 15:40:02 -0500

    Update issue

--- api/issue/mark.php
+++ api/issue/mark.php
@@ -10,9 +10,15 @@ if ($user != null) {
     if (isset($_GET['id']) && isset($_POST['newStatus'])) {
         $id = $_GET['id'];
         $newStatus = $_POST['newStatus'];
-        $statement = $db_conn->prepare("UPDATE `Issues` SET `status` = '$newStatus' WHERE `Issues`.`id` = $id;") or die("Cannot Set Status");
+        $statement = $db_conn->prepare("UPDATE `Issues` SET `status` = '$newStatus', updated = CURRENT_TIMESTAMP WHERE `Issues`.`id` = $id;") or die("Cannot Set Status");
         $statement->execute();
         $response['message'] = "Updated";
+
+        $statement2 = $db_conn->prepare("SELECT * FROM `Issues` where id = $id;") or die("Cannot Fetch");
+        $statement2->execute();
+        $result = $statement2->fetchAll(PDO::FETCH_ASSOC);
+        $response['issue'] = $result[0];
+
     } else {
         $response['message'] = "Not Updated";
     }
--- js/view-issue.js
+++ js/view-issue.js
@@ -41,12 +41,18 @@ const updateIssueStatus = (id, newStatus) => {
     fetch(httpUrl + '/api/issue/mark.php?id=' + id, {
         method: "POST",
         body: formData,
-    }).then(() => {
+    }).then(async (response) => {
         const issueStatusElement = document.getElementById('issue-status');
-        alert("Issue Status Updated")
         issueStatusElement.innerHTML = newStatus
-    }).catch(()=>{
-        alert("There was an error creating an issue")
+        const jsonResponse = await response.json().x;
+        const issue = jsonResponse.issue;
+        populateIssue(issue)
+        alert("Issue Status Updated")
+    }).catch((err) => {
+        if (err)
+        {
+            alert("There was an error creating an issue. With technical error "+err.toString())
+        }
     })
 }
 
@@ -79,20 +85,20 @@ const populateIssue = (issue) => {
 
     const issueCreated = document.getElementById('issue-created')
 
-    if( createdDate.getHours() >= 12){
-        issueCreated.innerHTML = `Issue Created On ${MONTHS[createdDate.getMonth()] + " " + createdDate.getDate()+", "+createdDate.getFullYear()+" at " + hour1 + ":" + (createdDate.getMinutes()<10? '0' : '') + createdDate.getMinutes() +"PM"} By ${membersMap.get(issue.created_by)}`
-    }if( updatedDate.getHours() < 12){
-        issueCreated.innerHTML = `Issue Created On ${MONTHS[createdDate.getMonth()] + " " + createdDate.getDate()+", "+createdDate.getFullYear()+" at " + hour1 + ":" + (createdDate.getMinutes()<10? '0' : '') + createdDate.getMinutes() +"AM"} By ${membersMap.get(issue.created_by)}`
+    if (createdDate.getHours() >= 12) {
+        issueCreated.innerHTML = `Issue Created On ${MONTHS[createdDate.getMonth()] + " " + createdDate.getDate() + ", " + createdDate.getFullYear() + " at " + hour1 + ":" + (createdDate.getMinutes() < 10 ? '0' : '') + createdDate.getMinutes() + "PM"} By ${membersMap.get(issue.created_by)}`
+    } if (updatedDate.getHours() < 12) {
+        issueCreated.innerHTML = `Issue Created On ${MONTHS[createdDate.getMonth()] + " " + createdDate.getDate() + ", " + createdDate.getFullYear() + " at " + hour1 + ":" + (createdDate.getMinutes() < 10 ? '0' : '') + createdDate.getMinutes() + "AM"} By ${membersMap.get(issue.created_by)}`
     }
 
     const issueUpdated = document.getElementById('issue-updated');
-    
-     if( updatedDate.getHours() >= 12){
-         issueUpdated.innerHTML = `Last updated on ${MONTHS[updatedDate.getMonth()] + " " + updatedDate.getDate() +", "+updatedDate.getFullYear()+" at " + hour + ":" + (updatedDate.getMinutes()<10? '0' : '') + updatedDate.getMinutes() +"PM"}`
-     }if( updatedDate.getHours() < 12){
-         issueUpdated.innerHTML = `Last updated on ${MONTHS[updatedDate.getMonth()] + " " + updatedDate.getDate() +", "+updatedDate.getFullYear()+" at " + hour + ":" + (updatedDate.getMinutes()<10? '0' : '') + updatedDate.getMinutes() +"AM"}`
-     }
-    
+
+    if (updatedDate.getHours() >= 12) {
+        issueUpdated.innerHTML = `Last updated on ${MONTHS[updatedDate.getMonth()] + " " + updatedDate.getDate() + ", " + updatedDate.getFullYear() + " at " + hour + ":" + (updatedDate.getMinutes() < 10 ? '0' : '') + updatedDate.getMinutes() + "PM"}`
+    } if (updatedDate.getHours() < 12) {
+        issueUpdated.innerHTML = `Last updated on ${MONTHS[updatedDate.getMonth()] + " " + updatedDate.getDate() + ", " + updatedDate.getFullYear() + " at " + hour + ":" + (updatedDate.getMinutes() < 10 ? '0' : '') + updatedDate.getMinutes() + "AM"}`
+    }
+
     const issueStatusElement = document.getElementById('issue-status');
     const issueTypeElement = document.getElementById('issue-type');
     const issueAssignedToElement = document.getElementById('issue-assigned-to');
