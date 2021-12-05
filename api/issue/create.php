ba880907abb7f81ae627fbf81c65b12a81d2b260
Colin Campbell <prontobol@gmail.com>
2021-12-04 21:14:15 -0500

    Http error handling

--- api/issue/create.php
+++ api/issue/create.php
@@ -6,8 +6,7 @@ session_start();
 $user = $_SESSION['user'];
 $response = [];
 
-if ($user != null)
-{
+if ($user != null) {
     if (
         isset($_POST['title']) &&
         isset($_POST['description']) &&
@@ -22,30 +21,30 @@ if ($user != null)
         $priority = $_POST['priority'];
         $status = $_POST['status'];
         $createdBy = $user['id'];
-    
-        $createIssueQuery = "INSERT INTO `Issues` (`id`, `title`, `description`, `type`, `priority`, `status`, `assigned_to`, `created_by`, `created`, `updated`) ".
-        "VALUES (NULL, :title , :description , :type, :priority, :status, :assignedTo, :createdBy, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);";
-    
+
+        $createIssueQuery = "INSERT INTO `Issues` (`id`, `title`, `description`, `type`, `priority`, `status`, `assigned_to`, `created_by`, `created`, `updated`) " .
+            "VALUES (NULL, :title , :description , :type, :priority, :status, :assignedTo, :createdBy, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);";
+
         $statement = $db_conn->prepare($createIssueQuery);
         $statement->execute([
             'title' => $title,
-            'description'=> $description,
+            'description' => $description,
             'type' => $type,
             'priority' => $priority,
             'status' => $status,
             'assignedTo' => $assignedTo,
             'createdBy' => $createdBy,
         ]);
-        
-        $response['message'] = 'Created'; 
-        
-    } else 
-    {
-        $response['message'] = 'Credentials Needed'; 
+
+        $response['message'] = 'Created';
+        http_response_code(201);
+    } else {
+        $response['message'] = 'Credentials Needed';
+        http_response_code(400);
     }
-} else 
-{
+} else {
     $response['message'] = 'No Login';
+    http_response_code(401);
 }
 
 echo json_encode($response);
